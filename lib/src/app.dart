fa2b645c57376f9eb0e83631be5f42344be1f500
iBlurBlur <tanakorn.ngam@gmail.com>
2021-08-17 14:13:02 +0700

    flow login and logout

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:hero_flutter/src/configs/routes/app_route.dart';
+import 'package:hero_flutter/src/constants/app_setting.dart';
+import 'package:hero_flutter/src/pages/home/home_page.dart';
 import 'package:hero_flutter/src/pages/login/login_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
@@ -9,7 +12,24 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: AppRoute().getAll,
-      home: LoginPage(),
+      home: _buildHomePage(),
     );
   }
+
+   FutureBuilder<SharedPreferences> _buildHomePage() => FutureBuilder<SharedPreferences>(
+      future: SharedPreferences.getInstance(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Container(
+            color: Colors.white,
+          );
+        }
+
+        final token = snapshot.data?.getString(AppSetting.token) ?? "";
+        if (token.isEmpty) {
+          return LoginPage();
+        }
+        return HomePage();
+      },
+    );
 }
--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:hero_flutter/src/configs/routes/app_route.dart';
+import 'package:hero_flutter/src/constants/app_setting.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -6,6 +10,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: CustomDrawer(),
       appBar: AppBar(
         title: Text('appbarTitle'),
       ),
@@ -20,6 +25,64 @@ class HomePage extends StatelessWidget {
         child: Icon(Icons.add),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+    );
+  }
+}
+
+class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          Spacer(),
+          ListTile(
+            leading: FaIcon(FontAwesomeIcons.signOutAlt),
+            title: Text('Log out'),
+            onTap: () => _logout(context),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _logout(BuildContext context) {
+    showDialog<void>(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext dialogContext) {
+        return AlertDialog(
+          title: Text('title'),
+          content: Text('dialogBody'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('cancel'),
+              onPressed: () {
+                Navigator.of(dialogContext).pop(); // Dismiss alert dialog
+              },
+            ),
+            TextButton(
+              child: Text(
+                'Log out',
+                style: TextStyle(
+                  color: Colors.red,
+                ),
+              ),
+              onPressed: () async {
+                Navigator.of(dialogContext).pop(); // Dismiss alert dialog
+                final pref = await SharedPreferences.getInstance();
+                pref.remove(AppSetting.token);
+                //pref.clear();
+                Navigator.pushNamedAndRemoveUntil(context, AppRoute.login, (route) => false);
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
