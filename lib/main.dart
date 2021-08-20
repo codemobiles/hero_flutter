20fd4fbd9133cff497c342045a2beaeaebc1ab25
iBlurBlur <tanakorn.ngam@gmail.com>
2021-08-20 14:32:30 +0700

    bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hero_flutter/src/app.dart';
+import 'package:hero_flutter/src/bloc/app_bloc_observer.dart';
 
 void main() {
+  Bloc.observer = AppBlocObserver();
   runApp(App());
 }
 
---
+++ lib/src/bloc/app_bloc_observer.dart
@@ -0,0 +1,15 @@
+import 'package:bloc/bloc.dart';
+
+class AppBlocObserver extends BlocObserver {
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    print(transition);
+    super.onTransition(bloc, transition);
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    print(error);
+    super.onError(bloc, error, stackTrace);
+  }
+}
